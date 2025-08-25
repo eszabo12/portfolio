bc2dd6bb88372deab17b60ad8cfcf7864b3b48db
Elle Szabo <szaboe18@gmail.com>
2025-08-25 15:37:03 -0400

    setup google analytics!

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
+import { GoogleAnalytics } from '@next/third-parties/google'
+
 import './globals.css';
 
 const geistSans = Geist({
@@ -79,6 +81,7 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+			      <GoogleAnalytics gaId="G-PDQXKNVQSX" />
 		</html>
 	);
 }
