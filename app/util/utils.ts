fd4d35e9626c3bac7382ee16f678f3e67abb87d4
Elle A. Szabo <elleszabo77@gmail.com>
2025-09-10 11:27:46 -0400

    clean up

--- app/util/utils.ts
+++
@@ -1,8 +0,0 @@
-function downloadPdfFromUrl( pdfUrl: string) {
-    const link = document.createElement('a');
-    link.href = pdfUrl;
-    link.download = 'Elle_Szabo_Resume.pdf'; 
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-}
