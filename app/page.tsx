514f520bf5fb06adc359a16df2be1e58c8668195
Elle Szabo <elleszabo77@gmail.com>
2025-09-29 16:29:02 -0400

    rem script

--- app/page.tsx
+++ app/page.tsx
@@ -15,34 +15,6 @@ export default function BackendPortfolio() {
 				strategy="afterInteractive"
 				src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
 			/>
-			<Script
-				id="gtag-init"
-				strategy="afterInteractive"
-				dangerouslySetInnerHTML={{
-					__html: `
-						window.dataLayer = window.dataLayer || [];
-						function gtag(){dataLayer.push(arguments);}
-						gtag('js', new Date());
-
-						(function() {
-							fetch('https://ipapi.co/json/')
-								.then(function(response) { return response.json(); })
-								.then(function(data) {
-									var zip = data.postal || '';
-									gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
-										page_path: window.location.pathname,
-										user_zip_code: zip
-									});
-								})
-								.catch(function() {
-									gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
-										page_path: window.location.pathname
-									});
-								});
-						})();
-					`,
-				}}
-			/>
 			<main className="min-h-screen bg-[#1A1A1A] text-white overflow-x-hidden">
 				<CustomCursor />
 
