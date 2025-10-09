ffb3ac9e342a79d285b1c6fd8b31173cf1b471f4
Elle Szabo <elleszabo77@gmail.com>
2025-10-09 15:18:31 -0400

    Typescript

--- app/pages/Constants.ts
+++ app/pages/Constants.ts
@@ -1,44 +1,6 @@
 
 export const projects = [
     {
-      img: '/codenames.jpg',
-      title: 'Codenames',
-      date: '2022',
-      description: [
-        'Programmed and trained an NLP agent to play Codenames, a popular word association game',
-        'Added a penalty into the evaluation for black card tokens',
-        'The first multi-agent (two-player) AI implementation of Codenames to my knowledge',
-      ],
-      skills: ['Python', 'OpenAI Gym', 'numpy'],
-      gh: true,
-      ghLink: 'https://github.com/eszabo12/codenames_project',
-    },
-    {
-      img: '/citibike.jpg',
-      title: 'iOS Rental Application',
-      date: '2020',
-      description: [
-        'Designed and developed Citibike-esque rental application in Swift using UIKit',
-        'Implemented backend with Heroku, map with Google API, and checkout with Stripe APIs',
-        'Developed for iOS Programming class',
-      ],
-      skills: ['Swift', 'UIKit', 'Heroku', 'Google Maps API', 'Stripe'],
-      gh: true,
-      ghLink: 'https://github.com/eszabo12/Mobile_iOS_Bird-esque_Application',
-    },
-    {
-      img: '/stream.png',
-      title: 'Streaming Chrome Extension',
-      date: '2022',
-      description: [
-        'Developed a chrome extension for HackSC that scrapes Twitch chat data and visualizes sentiment in real time',
-        'Connected nltk sentiment library via socketing to browser client',
-      ],
-      skills: ['JavaScript', 'Chrome Extension', 'nltk', 'Socket.io', 'Flask'],
-      gh: true,
-      ghLink: 'https://github.com/likhity/twitch-stream-chat-analyzer',
-    },
-    {
       img: '/report.gif',
       title: 'Report Improvements - Dripos',
       date: '2025',
@@ -51,17 +13,84 @@ export const projects = [
       gh: false,
     },
     {
-      img: '/lunar_lander.gif',
-      title: 'Pyribs',
-      date: '2022',
+      img: '/daily_insights.gif',
+      title: 'Daily Insights - Dripos',
+      date: '2025',
       description: [
-        'Contributed to open-source Python machine learning library for exploring the latent spaces in diffusion models',
-        'Library used by thousands of researchers worldwide'
+        'Designed and developed a mobile and desktop-responsive daily insights email template for Dripos, aggregating key business metrics and trends for coffee shop owners',
+        'Automated a high-throughput scheduled event for timely delivery to thousands of users',
+        'Tracked send and open rates with a MongoDB collection',
       ],
-      skills: ['Python', 'Jax'],
-      gh: true,
-      ghLink: 'https://github.com/icaros-usc/pyribs/pull/208',
+      skills: ['TypeScript', 'Node.js', 'AWS SMS', 'MySQL'],
+      gh: false,
     },
+    // {
+    //   img: '/stream.png',
+    //   title: 'Streaming Chrome Extension',
+    //   date: '2022',
+    //   description: [
+    //     'Developed a chrome extension for HackSC that scrapes Twitch chat data and visualizes sentiment in real time',
+    //     'Connected nltk sentiment library via socketing to browser client',
+    //   ],
+    //   skills: ['JavaScript', 'Chrome Extension', 'nltk', 'Socket.io', 'Flask'],
+    //   gh: true,
+    //   ghLink: 'https://github.com/likhity/twitch-stream-chat-analyzer',
+    // },
+    {
+      img: '/min-wage.jpeg',
+      title: 'min-wage npm package',
+      date: '2025',
+      description: [
+        'Published a Typescript npm package that scrapes live from the Department of Labor website ',
+        'Fetches the latest minimum wage data for all US states and territories for use in compliant payroll features',
+    ],
+      skills: ['TypeScript','cheerio', 'npm'],
+      gh: true,
+      ghLink: 'https://github.com/eszabo12/Minimum-Wage',
+    // buttons: [
+    //   {
+    //     label: 'Demo',
+    //     href: 'https://min-wage-demo.vercel.app/',
+    //     className: 'btn-blue'
+    //   }
+    // ],
+    },
+    {
+      img: '/Stripe-Logo.png',
+      title: 'Stripe SDK Extension',
+      date: '2024',
+      description: [
+        'Extended the Stripe Node.js SDK to enable advanced testing of payout triggers and webhook flows in a sandbox environment',
+        'Built custom scripts to simulate payout events, automate test scenarios, and validate integration with downstream financial systems'
+      ],
+      skills: ['TypeScript', 'Node.js', 'Stripe', 'Webhooks', 'Unit Testing'],
+      gh: true,
+      ghLink: 'https://github.com/eszabo12/stripe-cli/tree/elle/trigger',
+    },
+    {
+      img: '/citibike.jpg',
+      title: 'iOS Application',
+      date: '2020',
+      description: [
+        'Designed and developed Citibike-esque rental application in Swift',
+        'Implemented backend with Heroku, map with Google API, and checkout with Stripe APIs',
+      ],
+      skills: ['Swift', 'UIKit', 'Heroku', 'Google Maps API', 'Stripe'],
+      gh: true,
+      ghLink: 'https://github.com/eszabo12/Mobile_iOS_Bird-esque_Application',
+    },
+    // {
+    //   img: '/lunar_lander.gif',
+    //   title: 'Pyribs',
+    //   date: '2022',
+    //   description: [
+    //     'Contributed to open-source Python machine learning library for exploring the latent spaces in diffusion models',
+    //     'Library used by thousands of researchers worldwide'
+    //   ],
+    //   skills: ['Python', 'Jax'],
+    //   gh: true,
+    //   ghLink: 'https://github.com/icaros-usc/pyribs/pull/208',
+    // },
     // {
     //   img: '/oculus.jpg',
     //   title: 'Mind Palace',
@@ -87,54 +116,17 @@ export const projects = [
     //   ghLink: 'https://github.com/eszabo12/image_capturing_pipeline',
     // },
     {
-        img: '/daily_insights.gif',
-        title: 'Daily Insights - Dripos',
-        date: '2025',
-        description: [
-          'Designed and developed a mobile and desktop-responsive daily insights email template for Dripos, aggregating key business metrics and trends for coffee shop owners',
-          'Automated a high-throughput scheduled event for timely delivery to thousands of users',
-          'Tracked send and open rates with a MongoDB collection',
-        ],
-        skills: ['TypeScript', 'Node.js', 'AWS SMS', 'MySQL'],
-        gh: false,
-      },
-      {
-        img: '/flask.gif',
-        title: 'AI Research',
-        date: '2023',
-        description: [
-          'Innovated on the unique use of contrast sets in my first author AI + Robotics paper as an undergrad. Project highlighted by CoRL and later by NVIDIA Jetson Lab via the continuation of Abrar Anwar',
-          'As top 15% of workshop submissions, gave a spotlight presentation at The Conference on Robot Learning (CoRL) in Atlanta, Georgia October 2023'
-        ],
-        skills: ['Python', 'NLP'],
-        gh: true,
-        ghLink: 'https://github.com/eszabo12/VLN-CE',
-        buttons: [
-          {
-            label: 'Paper',
-            href: 'https://openreview.net/forum?id=uABEHp6tjy',
-            className: 'btn-blue'
-          }
-        ]
-      },
-    {
-      img: '/min-wage.jpeg',
-      title: 'min-wage npm package',
-      date: '2025',
+      img: '/codenames.jpg',
+      title: 'Codenames',
+      date: '2022',
       description: [
-        'Published a Typescript npm package that scrapes live from the Department of Labor website ',
-        'Fetches the latest minimum wage data for all US states and territories for use in compliant payroll features',
-    ],
-      skills: ['TypeScript','cheerio', 'npm'],
+        'Programmed and trained an NLP agent to play Codenames, a popular word association game',
+        'Added a penalty into the evaluation for black card tokens',
+        'The first multi-agent (two-player) AI implementation of Codenames to my knowledge',
+      ],
+      skills: ['Python', 'OpenAI Gym', 'numpy'],
       gh: true,
-      ghLink: 'https://github.com/eszabo12/Minimum-Wage',
-    // buttons: [
-    //   {
-    //     label: 'Demo',
-    //     href: 'https://min-wage-demo.vercel.app/',
-    //     className: 'btn-blue'
-    //   }
-    // ],
+      ghLink: 'https://github.com/eszabo12/codenames_project',
     },
     {
       img: '/folio.png',
@@ -149,18 +141,25 @@ export const projects = [
       gh: true,
       ghLink: 'https://github.com/eszabo12/portfolio',
     },
-    {
-      img: '/Stripe-Logo.png',
-      title: 'Stripe SDK Extension',
-      date: '2024',
-      description: [
-        'Extended the Stripe Node.js SDK to enable advanced testing of payout triggers and webhook flows in a sandbox environment',
-        'Built custom scripts to simulate payout events, automate test scenarios, and validate integration with downstream financial systems'
-      ],
-      skills: ['TypeScript', 'Node.js', 'Stripe', 'Webhooks', 'Unit Testing'],
-      gh: true,
-      ghLink: 'https://github.com/eszabo12/stripe-cli/tree/elle/trigger',
-    },
+    //   {
+    //     img: '/flask.gif',
+    //     title: 'AI Research',
+    //     date: '2023',
+    //     description: [
+    //       'Innovated on the unique use of contrast sets in my first author AI + Robotics paper as an undergrad. Project highlighted by CoRL and later by NVIDIA Jetson Lab via the continuation of Abrar Anwar',
+    //       'As top 15% of workshop submissions, gave a spotlight presentation at The Conference on Robot Learning (CoRL) in Atlanta, Georgia October 2023'
+    //     ],
+    //     skills: ['Python', 'NLP'],
+    //     gh: true,
+    //     ghLink: 'https://github.com/eszabo12/VLN-CE',
+    //     buttons: [
+    //       {
+    //         label: 'Paper',
+    //         href: 'https://openreview.net/forum?id=uABEHp6tjy',
+    //         className: 'btn-blue'
+    //       }
+    //     ]
+    //   },
   ];
 
   export const experiences = [
